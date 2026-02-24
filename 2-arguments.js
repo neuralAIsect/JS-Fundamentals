f6857efbb6e6d94940eb369148356ac4847bca99
neuralAIsect <openllots@gmail.com>
2026-02-24 17:29:00 +0100

    Write a script that prints a message depending of the number of arguments passed:

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(0);
+if(args.length === 0) {
+    console.log("No argument");
+}
+else if (args.length === 1) {
+    console.log("Argument found");
+}
+else {
+    console.log("aAguments found");
+}
+
+
